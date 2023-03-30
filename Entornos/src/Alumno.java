8a9cadba4b2b2e19a5f19315bdd17d3a993f90e4
elena <elena.garcia22@educa.madrid.org>
2023-03-30 09:35:01 +0200

    creacion de constructor

--- Entornos/src/Alumno.java
+++ Entornos/src/Alumno.java
@@ -3,5 +3,11 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private String dni;
+	
+	public Alumno(String nombre,String apellido,String dni) {
+		this.apellido=apellido;
+		this.nombre=nombre;
+		this.dni=dni;
+	}
 
 }
