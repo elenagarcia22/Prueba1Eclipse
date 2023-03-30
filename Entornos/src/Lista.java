8746d5be13914e6ca8d7d85227bcfa73bb11ee1d
elena <elena.garcia22@educa.madrid.org>
2023-03-30 09:40:24 +0200

    Creacion del alumno

--- Entornos/src/Lista.java
+++ Entornos/src/Lista.java
@@ -5,6 +5,7 @@ public class Lista {
 		// TODO Esbozo de método generado automáticamente
 		System.out.println("Listado de alumnos 1ºDAW");
 		System.out.println("Alumno 1:");
+		Alumno a1=new Alumno("Elena","Garcia","54679473J");
 
 	}
 
