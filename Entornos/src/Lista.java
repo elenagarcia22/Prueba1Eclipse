058eacd706d6563334c532040435e1a5699f5dbb
elena <elena.garcia22@educa.madrid.org>
2023-03-30 09:28:48 +0200

    mensaje inferior

--- Entornos/src/Lista.java
+++ Entornos/src/Lista.java
@@ -4,6 +4,7 @@ public class Lista {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		System.out.println("Listado de alumnos 1ºDAW");
+		System.out.println("Alumno 1:");
 
 	}
 
